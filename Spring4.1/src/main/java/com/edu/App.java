27ea62e0e430618aeac5019049efaf51dd1bc35a
ykumar <ykumar@ykumar-DL-IN.usa.firstrain.com>
2016-04-30 01:15:06 +0530

    test

--- Spring4.1/src/main/java/com/edu/App.java
+++ Spring4.1/src/main/java/com/edu/App.java
@@ -1,14 +1,26 @@
 package com.edu;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * Starts application web server.
  */
-@SpringBootApplication
-public class App {
-  public static void main(String[] args) {
-    SpringApplication.run(App.class, args);
-  }
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class App extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(App.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
 }
